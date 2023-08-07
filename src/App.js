b181a884a504d3f27ef6d66f80285a6851b02125
Daniel Martinez <martinezh.dani@gmail.com>
2023-08-07 14:57:27 -0600

    add container for SPA

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import { BrowserRouter } from "react-router-dom";
 import NavbarLink from "./Components/navbarLink";
 import { AuthProvider } from "./Context/authContext";
 import SPA from "./SPA/SPA";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding-top: 10px;
+
+`;
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <NavbarLink />
-        <SPA />
+        <Container>
+          <SPA />
+        </Container>
       </AuthProvider>
     </BrowserRouter>
   );
